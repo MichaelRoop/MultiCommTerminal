fb5a8acab47eb861ab02aace62dfde00e151dcf0
michael@roop.ca <michael@roop.ca>
2020-03-03 09:49:36 -0400

    Initial Git checkin

---
+++ MultiCommData/UserDisplay/NameKey.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiCommData.UserDisplay {
+
+    /// <summary>Group a communication message and its display value</summary>
+    public class NameKey {
+
+        /// <summary>String displayed to user</summary>
+        public string Display { get; set; }
+        /// <summary>String sent or received to and from communication channel</summary>
+        public string Msg { get; set; }
+
+    }
+}
