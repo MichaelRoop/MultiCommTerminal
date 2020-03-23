7bd42617af6e8e56c79f968fb287811a9a38539e
michael@roop.ca <michael@roop.ca>
2020-03-23 17:09:34 -0300

    Get the Bluetooth LE working correctly using the Watcher. I think

--- BluetoothLE.Win32/BluetoothLEImplWin32.cs
+++ BluetoothLE.Win32/BluetoothLEImplWin32.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
 using Windows.Devices.Enumeration;
 
 namespace BluetoothLE.Win32 {
@@ -18,6 +20,170 @@ namespace BluetoothLE.Win32 {
 
 
     public class BluetoothLEImplWin32 : IBTInterface {
+
+
+
+        /// <summary>Interface call
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        List<BTDeviceInfo> IBTInterface.DiscoverDevices() {
+            this.DoLEWatcherSearch();
+            return this.infoListMain;
+
+
+            //return this.DiscoverDevices1();
+            //return this.DiscoverDevices3();
+
+            ////this.infoListMain.Clear();
+            ////Task<List<BTDeviceInfo>> task = this.GetBTDevices().Wait(4000);
+
+            //Task<List<BTDeviceInfo>> task = Task.Run<List<BTDeviceInfo>>(async () => await GetBTDevices());
+
+            //var result = this.GetBTDevices().Wait(4000);
+
+
+            //List<BTDeviceInfo> infoList = new List<BTDeviceInfo>();
+            //foreach (BTDeviceInfo d in this.infoListMain) {
+            //    BTDeviceInfo info = new BTDeviceInfo() {
+            //        Name = d.Name,
+            //        Connected = d.Connected,
+            //        Authenticated = d.Authenticated,
+            //        Address = d.Address,
+            //        DeviceClassInt = d.DeviceClassInt,
+            //        DeviceClassName = d.DeviceClassName,
+            //        ServiceClassInt = d.ServiceClassInt,
+            //        ServiceClassName = d.ServiceClassName,
+            //    };
+            //    infoList.Add(info);
+            //}
+
+
+
+
+            //return infoList;
+        }
+
+        #region Watcher approach
+
+        DeviceWatcher devWatcher = null;
+
+        ManualResetEvent done = new ManualResetEvent(false);
+
+
+        private void DoLEWatcherSearch() {
+            lock (this.infoListMain) {
+                this.infoListMain.Clear();
+            }
+            this.SetupWatcher();
+            this.SetupEvents();
+            this.done.Reset();
+            this.devWatcher.Start();
+            // problem - the enumeration end never happens. Cannot set too high
+            if (this.done.WaitOne(30000)) {
+
+            }
+            this.TearDownEvents();
+            this.devWatcher.Stop();
+        }
+
+        private void SetupWatcher() {
+            // https://stackoverflow.com/questions/48909496/aqs-syntax-when-providing-a-filter-for-ble-scanning-in-uwp
+            // https://github.com/microsoft/Windows-universal-samples/blob/master/Samples/DeviceEnumerationAndPairing/cs/Scenario7_DeviceInformationKind.xaml.cs
+            // https://github.com/microsoft/Windows-universal-samples/blob/master/Samples/DeviceEnumerationAndPairing/cs/DeviceWatcherHelper.cs
+
+            if (this.devWatcher == null) {
+                // With properties only get 4
+                string[] prop = { 
+                    "System.Devices.Aep.DeviceAddress",
+                    "System.Devices.Aep.IsConnected",
+                    "System.Devices.Aep.Bluetooth.Le.IsConnectable",
+                    "System.Devices.Aep.IsPaired"
+                };
+                #region failed experiments
+                //// Even with null properties I get 4
+                //this.devWatcher = DeviceInformation.CreateWatcher(
+                //    BluetoothLEDevice.GetDeviceSelectorFromPairingState(false), prop, .AssociationEndpoint);
+
+                //// Gets Ergonomic keyboard only 
+                //this.devWatcher = DeviceInformation.CreateWatcher(
+                //    BluetoothLEDevice.GetDeviceSelector(), prop, DeviceInformationKind.AssociationEndpoint);
+
+                // 1413 devices, duplicates, etc
+                //this.devWatcher = DeviceInformation.CreateWatcher(DeviceClass.All);
+
+                // 0 devices
+                //this.devWatcher = DeviceInformation.CreateWatcher("System.Devices.Aep.DeviceAddress:<>[]");
+
+                // 0 devices
+                //this.devWatcher = DeviceInformation.CreateWatcher("System.Devices.Aep.DeviceAddress:<>[]", reqProperties);
+                #endregion
+
+                // with AQS Get 23 if I use properties, if null properties only 22
+                string aqsBTAddressNotNull = "System.Devices.Aep.DeviceAddress:<>[]";
+                this.devWatcher = DeviceInformation.CreateWatcher(
+                    aqsBTAddressNotNull, prop, DeviceInformationKind.AssociationEndpoint);
+            }
+        }
+
+        private void SetupEvents() {
+            devWatcher.Added += DevWatcher_Added;
+            devWatcher.Updated += DevWatcher_Updated;
+            devWatcher.Removed += DevWatcher_Removed;
+            devWatcher.EnumerationCompleted += DevWatcher_EnumerationCompleted;
+            devWatcher.Stopped += DevWatcher_Stopped;
+        }
+
+        private void TearDownEvents() {
+            devWatcher.Added -= DevWatcher_Added;
+            devWatcher.Updated -= DevWatcher_Updated;
+            devWatcher.Removed -= DevWatcher_Removed;
+            devWatcher.EnumerationCompleted -= DevWatcher_EnumerationCompleted;
+            devWatcher.Stopped -= DevWatcher_Stopped;
+        }
+
+
+
+
+        private void DevWatcher_Stopped(DeviceWatcher sender, object args) {
+            //throw new NotImplementedException();
+        }
+
+        private void DevWatcher_EnumerationCompleted(DeviceWatcher sender, object args) {
+            this.done.Set();
+        }
+
+        private void DevWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args) {
+            //throw new NotImplementedException();
+        }
+
+        private void DevWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args) {
+            //throw new NotImplementedException();
+        }
+
+        private void DevWatcher_Added(DeviceWatcher sender, DeviceInformation args) {
+            lock (this.infoListMain) { // Locking the container causes the list not to populate past first
+                if (args.Name.Length > 0) {
+                    this.infoListMain.Add(new BTDeviceInfo() {
+                        Name = args.Name,
+                        Address = args.Id,
+                        // Does not apply for bluetooth LE
+                        Authenticated = true,
+                        // the aqs query only returns the connected ones
+                        Connected = true, 
+                        DeviceClassInt = 0,
+                        DeviceClassName = "0",
+                        ServiceClassInt = 0,
+                        ServiceClassName = "0",
+                    });
+                }
+            }
+        }
+
+        #endregion
+
+
+        #region Async Calls with wait. Quarky
 
         List<BTDeviceInfo> infoListMain = new List<BTDeviceInfo>();
 
@@ -112,64 +278,6 @@ namespace BluetoothLE.Win32 {
             return infoListMain;
         }
 
-
-
-
-        List<BTDeviceInfo> IBTInterface.DiscoverDevices() {
-            //return this.DiscoverDevices1();
-            return this.DiscoverDevices3();
-
-            ////this.infoListMain.Clear();
-            ////Task<List<BTDeviceInfo>> task = this.GetBTDevices().Wait(4000);
-
-            //Task<List<BTDeviceInfo>> task = Task.Run<List<BTDeviceInfo>>(async () => await GetBTDevices());
-
-            //var result = this.GetBTDevices().Wait(4000);
-
-
-            //List<BTDeviceInfo> infoList = new List<BTDeviceInfo>();
-            //foreach (BTDeviceInfo d in this.infoListMain) {
-            //    BTDeviceInfo info = new BTDeviceInfo() {
-            //        Name = d.Name,
-            //        Connected = d.Connected,
-            //        Authenticated = d.Authenticated,
-            //        Address = d.Address,
-            //        DeviceClassInt = d.DeviceClassInt,
-            //        DeviceClassName = d.DeviceClassName,
-            //        ServiceClassInt = d.ServiceClassInt,
-            //        ServiceClassName = d.ServiceClassName,
-            //    };
-            //    infoList.Add(info);
-            //}
-
-
-
-
-            //return infoList;
-        }
-
-        async Task GetBTDevices3() {
-            //aqs syntax
-            //https://docs.microsoft.com/en-us/uwp/api/windows.devices.enumeration.deviceinformation.createwatcher
-
-            //this.found = await DeviceInformation.FindAllAsync(DeviceClass.All);
-
-            // Has IP address "System.Devices.IpAddress:<>[]"
-            // No IP address "System.Devices.IpAddress:=[]" - however it filters out the itead. Maybe because it has an ethernet shield?
-            // Bluetooth LE address "System.Devices.Aep.DeviceAddress:<>[]"
-            // LE Is Connected "System.Devices.Aep.IsConnected:true"  - does not work
-
-            this.found = await DeviceInformation.FindAllAsync(
-                "System.Devices.Aep.DeviceAddress:<>[]", null, DeviceInformationKind.Device);
-
-            // for a watcher
-            //"System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
-
-
-
-        }
-
-
         async Task GetBTDevices1() {
             //await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync();
             //eviceClass dc = new DeviceClass();
@@ -207,6 +315,23 @@ namespace BluetoothLE.Win32 {
 
             }
 
+        async Task GetBTDevices3() {
+            //aqs syntax
+            //https://docs.microsoft.com/en-us/uwp/api/windows.devices.enumeration.deviceinformation.createwatcher
+
+            //this.found = await DeviceInformation.FindAllAsync(DeviceClass.All);
+
+            // Has IP address "System.Devices.IpAddress:<>[]"
+            // No IP address "System.Devices.IpAddress:=[]" - however it filters out the itead. Maybe because it has an ethernet shield?
+            // Bluetooth LE address "System.Devices.Aep.DeviceAddress:<>[]"
+            // LE Is Connected "System.Devices.Aep.IsConnected:true"  - does not work
+
+            this.found = await DeviceInformation.FindAllAsync(
+                "System.Devices.Aep.DeviceAddress:<>[]", null, DeviceInformationKind.Device);
+
+            // for a watcher
+            //"System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+        }
 
         async Task<List<BTDeviceInfo>> GetBTDevices2() {
             //await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync();
@@ -249,11 +374,9 @@ namespace BluetoothLE.Win32 {
 
         }
 
+        #endregion
 
 
 
-
-
-
-        }
     }
+}
