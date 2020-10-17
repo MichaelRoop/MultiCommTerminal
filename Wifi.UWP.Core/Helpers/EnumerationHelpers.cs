1821ed9bf4851677970c2f3add1797b4b1192292
michael@roop.ca <michael@roop.ca>
2020-10-17 19:43:18 -0300

    Get more WIFI info

--- Wifi.UWP.Core/Helpers/EnumerationHelpers.cs
+++ Wifi.UWP.Core/Helpers/EnumerationHelpers.cs
@@ -4,6 +4,7 @@ using System.Text;
 using VariousUtils.Net;
 using WifiCommon.Net.Enumerations;
 using Windows.Devices.WiFi;
+using Windows.Networking.Connectivity;
 
 namespace Wifi.UWP.Core.Helpers {
 
@@ -12,6 +13,24 @@ namespace Wifi.UWP.Core.Helpers {
         public static NetworkKind Convert(this WiFiNetworkKind kind) {
             return EnumHelpers.ToEnum<NetworkKind>(
                 EnumHelpers.ToInt(kind));
+        }
+
+
+        public static WifiPhyPhysicalLayerKind Convert(this WiFiPhyKind phyKind) {
+            return EnumHelpers.ToEnum<WifiPhyPhysicalLayerKind>(
+                EnumHelpers.ToInt(phyKind));
+        }
+
+
+        public static NetAuthenticationType Convert (this NetworkAuthenticationType authenticationType) {
+            return EnumHelpers.ToEnum<NetAuthenticationType>(
+                EnumHelpers.ToInt(authenticationType));
+        }
+
+
+        public static NetEncryptionType Convert(this NetworkEncryptionType encryptionType) {
+            return EnumHelpers.ToEnum<NetEncryptionType>(
+                EnumHelpers.ToInt(encryptionType));
         }
 
     }
--- Wifi.UWP.Core/WifiImpleUwpDiscover.cs
+++ Wifi.UWP.Core/WifiImpleUwpDiscover.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Wifi.UWP.Core.Helpers;
 using WifiCommon.Net.DataModels;
@@ -16,6 +15,8 @@ namespace Wifi.UWP.Core {
         #region Data
 
         private WiFiAdapter wifiAdapter = null;
+        private WifiAdapterInfo adapterInfo = new WifiAdapterInfo();
+        IReadOnlyList<WiFiAvailableNetwork> networks = new List<WiFiAvailableNetwork>();
 
         #endregion
 
@@ -46,56 +47,74 @@ namespace Wifi.UWP.Core {
                     this.OnError?.Invoke(this, new WifiError(WifiErrorCode.NoAdapters));
                     return;
                 }
-                else {
-                    if (adapters.Count > 1) {
-                        this.log.Warning(9999, () => string.Format("{0} WIFI adapters. Selecting first", adapters.Count));
-                    }
-                    this.wifiAdapter = adapters[0];
-                    this.DisplayAdapterValues(this.wifiAdapter);
+
+                if (adapters.Count > 1) {
+                    this.log.Warning(9999, () => string.Format("{0} WIFI adapters. Selecting first", adapters.Count));
                 }
+                this.wifiAdapter = adapters[0];
             }
 
+            this.InitAdapterInfo(this.wifiAdapter, this.adapterInfo);
+            await this.ScanForNetworks(this.wifiAdapter, this.adapterInfo);
 
+            // TODO - change to send up the whole adapter
+            this.DiscoveredNetworks?.Invoke(this, this.adapterInfo.Networks);
 
-            await this.wifiAdapter.ScanAsync();
-            List<WifiNetworkInfo> networks = new List<WifiNetworkInfo>();
-            // We will not return those with blank SSID
-            foreach (var net in wifiAdapter.NetworkReport.AvailableNetworks) {
-                if (net.Ssid.Length > 0) {
-                    this.log.Info("", () => string.Format("Found {0}", net.Ssid));
-                    networks.Add(new WifiNetworkInfo() {
-                        SSID = net.Ssid,
-                        Kind = net.NetworkKind.Convert(),
-                    });
-                }
-            }
-
-            this.log.Info("", () => string.Format("Found {0} networks", networks.Count));
-            this.log.Info("", () => string.Format("DiscoveredNetworks={0}", this.DiscoveredNetworks == null));
-
-            this.DiscoveredNetworks?.Invoke(this, networks);
-
+            #region junk
             //this.log.Info("DoDiscovery", () => string.Format("Count {0}", result.Count));
             //foreach(var adapter in result) {
             //    this.log.Info("DoDiscovery", () => string.Format("Available networks {0}", adapter.NetworkReport.AvailableNetworks.Count));
-
-                //    //// This gets all the available networks
-                //    //await adapter.ScanAsync();
-                //    //this.DisplayNetworkReport(adapter.NetworkReport);
-
-                //    // Yes, this successfuly connects with passed in credentials
-                //    //await this.ConnectToNetwork(adapter, "MikieArduinoWifi", "1234567890");
-                //}
+            //    //// This gets all the available networks
+            //    //await adapter.ScanAsync();
+            //    //this.DisplayNetworkReport(adapter.NetworkReport);
+            //    // Yes, this successfuly connects with passed in credentials
+            //    //await this.ConnectToNetwork(adapter, "MikieArduinoWifi", "1234567890");
+            //}
+            #endregion
         }
 
-        private void DisplayAdapterValues(WiFiAdapter adapter) {
-            this.log.InfoEntry("ListAdapterValues");
-            // TODO fill in object for use in display
-            this.log.Info("ListAdapterValues", () => string.Format("  IATA Type: {0}", adapter.NetworkAdapter.IanaInterfaceType.ToEnum()));
-            this.log.Info("ListAdapterValues", () => string.Format(" Max BPS In: {0}", adapter.NetworkAdapter.InboundMaxBitsPerSecond));
-            this.log.Info("ListAdapterValues", () => string.Format("Max BPS Out: {0}", adapter.NetworkAdapter.OutboundMaxBitsPerSecond));
-            this.log.Info("ListAdapterValues", () => string.Format(" Adapter Id: {0}", adapter.NetworkAdapter.NetworkAdapterId.ToString()));
-            this.log.Info("ListAdapterValues", () => string.Format(" Network Id: {0}", adapter.NetworkAdapter.NetworkItem.NetworkId.ToString()));
+
+        private async Task ScanForNetworks(WiFiAdapter adapter, WifiAdapterInfo info) {
+            // TODO - will need a list of available UWP network objects to be able to find fof connection - or use the UWP adapter
+            await adapter.ScanAsync();
+            this.networks = this.wifiAdapter.NetworkReport.AvailableNetworks;
+            info.Networks = new List<WifiNetworkInfo>();
+            // We will not return those with blank SSID
+            foreach (var net in this.networks) {
+                if (net.Ssid.Length > 0) {
+                    WifiNetworkInfo netInfo = new WifiNetworkInfo() {
+                        SSID = net.Ssid,
+                        Kind = net.NetworkKind.Convert(),
+                        ChanneCenterFrequencyKlhz = net.ChannelCenterFrequencyInKilohertz,
+                        BeaconInterval = net.BeaconInterval,
+                        MacAddress_BSSID = net.Bssid,
+                        RssiInDecibleMilliwatts = net.NetworkRssiInDecibelMilliwatts,
+                        PhysicalLayerKind = net.PhyKind.Convert(),
+                        AuthenticationType = net.SecuritySettings.NetworkAuthenticationType.Convert(),
+                        EncryptionType = net.SecuritySettings.NetworkEncryptionType.Convert(),
+                        IsWifiDirect = net.IsWiFiDirect,
+                        SignalStrengthInBars = net.SignalBars,
+                        UpTime = net.Uptime,
+                    };
+
+                    this.DumpNetworkInfo(netInfo);
+                    info.Networks.Add(netInfo);
+
+
+
+                }
+            }
+            this.log.Info("ScanForNetworks", () => string.Format("Found {0} networks", info.Networks.Count));
+        }
+
+
+        private void InitAdapterInfo(WiFiAdapter adapter, WifiAdapterInfo info) {
+            this.log.InfoEntry("InitAdapterInfo");
+            info.AdapterId = adapter.NetworkAdapter.NetworkAdapterId;
+            info.IanaType = adapter.NetworkAdapter.IanaInterfaceType.ToEnum();
+            info.MaxBitsPerSecondIn = adapter.NetworkAdapter.InboundMaxBitsPerSecond;
+            info.MaxBitsPerSecondOut = adapter.NetworkAdapter.OutboundMaxBitsPerSecond;
+            this.DumpAdapterInfo(info);
         }
 
 
@@ -141,7 +160,33 @@ namespace Wifi.UWP.Core {
         }
 
 
+        #region Debug dump methods
 
+        private void DumpAdapterInfo(WifiAdapterInfo info) {
+            this.log.Info("DumpAdapterInfo", () => string.Format("  IANA Type: {0}", info.IanaType));
+            this.log.Info("DumpAdapterInfo", () => string.Format(" Max BPS In: {0}", info.MaxBitsPerSecondIn));
+            this.log.Info("DumpAdapterInfo", () => string.Format("Max BPS Out: {0}", info.MaxBitsPerSecondOut));
+            this.log.Info("DumpAdapterInfo", () => string.Format(" Adapter Id: {0}", info.AdapterId.ToString()));
+        }
+
+
+        private void DumpNetworkInfo(WifiNetworkInfo info) {
+            this.log.Info("DumpNetworkInfo", () => string.Format("------------------------------- {0} --------------------------------",info.SSID));
+            this.log.Info("DumpNetworkInfo", () => string.Format("                    SSID: {0}", info.SSID));
+            this.log.Info("DumpNetworkInfo", () => string.Format("             Wifi Direct: {0}", info.IsWifiDirect));
+            this.log.Info("DumpNetworkInfo", () => string.Format("                    Kind: {0}", info.Kind));
+            this.log.Info("DumpNetworkInfo", () => string.Format("                     MAC: {0}", info.MacAddress_BSSID));
+            this.log.Info("DumpNetworkInfo", () => string.Format("     Authentication Type: {0}", info.AuthenticationType));
+            this.log.Info("DumpNetworkInfo", () => string.Format("         Encryption Type: {0}", info.EncryptionType));
+            this.log.Info("DumpNetworkInfo", () => string.Format("         Beacon Interval: {0}", info.BeaconInterval));
+            this.log.Info("DumpNetworkInfo", () => string.Format("Center Channel Frequency: {0}", info.ChanneCenterFrequencyKlhz));
+            this.log.Info("DumpNetworkInfo", () => string.Format("          Physical Layer: {0}", info.PhysicalLayerKind));
+            this.log.Info("DumpNetworkInfo", () => string.Format("           RSSI Decibles: {0}", info.RssiInDecibleMilliwatts));
+            this.log.Info("DumpNetworkInfo", () => string.Format("    Signal Strength Bars: {0}", info.SignalStrengthInBars));
+            this.log.Info("DumpNetworkInfo", () => string.Format("                 Up Time: {0}", info.UpTime));
+        }
+
+        #endregion
 
     }
 }
