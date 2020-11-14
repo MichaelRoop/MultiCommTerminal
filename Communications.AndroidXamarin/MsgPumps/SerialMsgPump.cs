6b18c0bb9b71735799953bc49cd9a3e729180c8a
michael@roop.ca <michael@roop.ca>
2020-11-14 13:57:20 -0400

    Android socket message pump started

---
+++ Communications.AndroidXamarin/MsgPumps/SerialMsgPump.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommunicationStack.Net.interfaces;
+using CommunicationStack.Net.DataModels;
+using System.Threading.Tasks;
+
+namespace Communications.AndroidXamarin.MsgPumps {
+
+    public class SocketConnData {
+
+
+    }
+
+    public class SerialMsgPump : IMsgPump<SocketConnData> {
+        public bool Connected => throw new NotImplementedException();
+
+        public event EventHandler<MsgPumpResults> MsgPumpConnectResultEvent;
+        public event EventHandler<byte[]> MsgReceivedEvent;
+
+        public void ConnectAsync(SocketConnData paramsObj) {
+            throw new NotImplementedException();
+        }
+
+        public Task ConnectAsync2(SocketConnData paramsObj) {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect() {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAsync(byte[] msg) {
+            throw new NotImplementedException();
+        }
+    }
+}
