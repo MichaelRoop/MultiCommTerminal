df5c35c524041d096b5a35cf70e1da0b0115ba65
michael@roop.ca <michael@roop.ca>
2020-11-18 20:50:54 -0400

    Run time privileges

---
+++ MultiCommTerminal.AndroidXamarin/PermissionObjects/ReadWriteStoragePermission.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static Xamarin.Essentials.Permissions;
+
+namespace MultiCommTerminal.AndroidXamarin.PermissionObjects {
+
+    public class ReadWriteStoragePermission : BasePlatformPermission {
+
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)> {
+            (Android.Manifest.Permission.ReadExternalStorage, true),
+            (Android.Manifest.Permission.WriteExternalStorage, true)
+        }.ToArray();
+
+    }
+}
