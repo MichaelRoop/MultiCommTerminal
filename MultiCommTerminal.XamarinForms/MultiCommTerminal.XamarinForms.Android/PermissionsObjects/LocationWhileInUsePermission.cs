a6472132779e48e26db8933142634af4873fe6fa
michael@roop.ca <michael@roop.ca>
2020-11-27 15:13:06 -0400

    More cleanup

--- MultiCommTerminal.XamarinForms/MultiCommTerminal.XamarinForms.Android/PermissionsObjects/LocationWhileInUsePermission.cs
+++ MultiCommTerminal.XamarinForms/MultiCommTerminal.XamarinForms.Android/PermissionsObjects/LocationWhileInUsePermission.cs
@@ -1,18 +1,11 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using MultiCommTerminal.XamarinForms.interfaces;
-using System;
+﻿using MultiCommTerminal.XamarinForms.interfaces;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace MultiCommTerminal.XamarinForms.Droid.PermissionsObjects {
 
+    /// <summary>Android implementation to check and request WIFI permissions</summary>
     public class LocationWhileInUsePermission : Xamarin.Essentials.Permissions.BasePlatformPermission, ILocationWhileInUsePermission {
+
         public override (string androidPermission, bool isRuntime)[] 
             RequiredPermissions => new List<(string androidPermission, bool isRuntime)> {
             (Android.Manifest.Permission.AccessFineLocation, true),
@@ -22,31 +15,5 @@ namespace MultiCommTerminal.XamarinForms.Droid.PermissionsObjects {
         }.ToArray();
 
     }
-
-
-
-#if GIERUWOEUOIEUOI
-
-https://forums.xamarin.com/discussion/165944/asking-user-for-permission
-You can create your own service to ask for permissions.
-Use Dependency Service to explicitly ask for permissions. If you do not know what is Dependency Service please check the Microsoft Docs.
-For Android
-
- public void RequestLocationPermission()
-    {
-            if (ContextCompat.CheckSelfPermission(Application.Context, Manifest.Permission.AccessFineLocation) != Permission.Granted)
-            {
-                MainActivity.Activity.RequestPermissions(new String[] { Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessCoarseLocation }, MainActivity.LocationPermissionRequestCode);
-            }
-
-            LocationManager locationManager = (LocationManager)Application.Context.GetSystemService(Context.LocationService);
-            if (!locationManager.IsProviderEnabled(LocationManager.GpsProvider))
-            {
-                Application.Context.StartActivity(new Intent(Android.Provider.Settings.ActionLocat‌​ionSourceSettings));
-            }
-
-    }
-
-#endif
 
 }
--- MultiCommTerminal.XamarinForms/MultiCommTerminal.XamarinForms/interfaces/ICloseApplication.cs
+++ MultiCommTerminal.XamarinForms/MultiCommTerminal.XamarinForms/interfaces/ICloseApplication.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace MultiCommTerminal.XamarinForms.interfaces {
 
+
     //https://stackoverflow.com/questions/29257929/how-to-terminate-a-xamarin-application
+    /// <summary>Interface to close OS bound Apps</summary>
     public interface ICloseApplication {
+
         void CloseApp();
+
     }
+
 }
