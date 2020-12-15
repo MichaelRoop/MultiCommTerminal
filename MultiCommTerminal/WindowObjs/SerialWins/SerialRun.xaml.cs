d10d49339a734e60986f103dd02c2251399dfc49
michael@roop.ca <michael@roop.ca>
2020-12-15 14:59:58 -0400

    Win new serial run

---
+++ MultiCommTerminal/WindowObjs/SerialWins/SerialRun.xaml.cs
@@ -0,0 +1,157 @@
+﻿using CommunicationStack.Net.DataModels;
+using CommunicationStack.Net.Enumerations;
+using MultiCommTerminal.NetCore.DependencyInjection;
+using MultiCommTerminal.NetCore.WPF_Helpers;
+using SerialCommon.Net.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfHelperClasses.Core;
+
+namespace MultiCommTerminal.NetCore.WindowObjs.SerialWins {
+
+    /// <summary>Interaction logic for SerialRun.xaml</summary>
+    public partial class SerialRun : Window {
+
+
+        private Window parent = null;
+        private SerialDeviceInfo selectedSerial = null;
+
+
+        public SerialRun(Window parent) {
+            this.parent = parent;
+            InitializeComponent();
+
+            WPF_ControlHelpers.CenterChild(parent, this);
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+
+            this.ui.ExitClicked += this.OnUiExit;
+            this.ui.ConnectCicked += this.OnUiConnect;
+            this.ui.DiscoverClicked += this.OnUiDiscover;
+            this.ui.DisconnectClicked += this.OnUiDisconnect;
+            this.ui.SendClicked += this.OnUiSend;
+            DI.Wrapper.OnSerialConfigRequest += this.onSerialConfigRequest;
+            DI.Wrapper.SerialOnError += this.onSerialError;
+            DI.Wrapper.Serial_BytesReceived += this.bytesReceived;
+            DI.Wrapper.OnSerialConnectionAttemptCompleted += this.onSerialConnectionAttemptCompleted;
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            WPF_ControlHelpers.CenterChild(parent, this);
+            this.ui.OnLoad(this.parent);
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            this.ui.ExitClicked -= this.OnUiExit;
+            this.ui.ConnectCicked -= this.OnUiConnect;
+            this.ui.DiscoverClicked -= this.OnUiDiscover;
+            this.ui.DisconnectClicked -= this.OnUiDisconnect;
+            this.ui.SendClicked -= this.OnUiSend;
+            DI.Wrapper.OnSerialConnectionAttemptCompleted -= this.onSerialConnectionAttemptCompleted;
+            DI.Wrapper.OnSerialConfigRequest -= this.onSerialConfigRequest;
+            DI.Wrapper.SerialOnError -= this.onSerialError;
+            DI.Wrapper.Serial_BytesReceived -= this.bytesReceived;
+            this.ui.OnClosing();
+        }
+
+
+        /// <summary>Connect the style mouse grab on title bar</summary>
+        public override void OnApplyTemplate() {
+            this.BindMouseDownToCustomTitleBar();
+            base.OnApplyTemplate();
+        }
+
+
+        #region Serial event handlers
+
+        private void bytesReceived(object sender, string msg) {
+            this.ui.AddResponse(msg);
+        }
+
+
+        private void onSerialConnectionAttemptCompleted(object sender, MsgPumpResults e) {
+            this.Dispatcher.Invoke(() => {
+                this.ui.IsBusy = false;
+                if (e.Code == MsgPumpResultCode.Connected) {
+                    this.ui.SetConnected();
+                }
+                else {
+                    App.ShowMsg(string.Format("{0} '{1}'", e.Code, e.ErrorString));
+                }
+            });
+        }
+
+
+        private void onSerialError(object sender, SerialUsbError e) {
+            this.Dispatcher.Invoke(() => {
+                this.ui.IsBusy = false;
+                App.ShowMsgTitle(e.PortName, string.Format("{0} '{1}'", e.Code, e.PortName, e.Message));
+            });
+        }
+
+
+        /// <summary>Raises popup if no saved Serial configurations for port</summary>
+        private void onSerialConfigRequest(object sender, SerialDeviceInfo e) {
+            this.Dispatcher.Invoke(() => {
+                this.ui.IsBusy = false;
+                DeviceEdit_USB.ShowBox(this, e);
+                this.ui.IsBusy = true;
+            });
+        }
+
+        #endregion
+
+        #region UI Event handlers
+
+        private void OnUiExit(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void OnUiDiscover(object sender, EventArgs e) {
+            this.Title = "USB";
+            this.selectedSerial = SerialSelectUSB.ShowBox(this);
+            if (this.selectedSerial != null) {
+                this.Title = this.selectedSerial.Name;
+            }
+        }
+
+
+        private void OnUiConnect(object sender, EventArgs e) {
+            if (this.selectedSerial == null) {
+                this.OnUiDiscover(sender, e);
+            }
+            if (this.selectedSerial != null) {
+                this.ui.IsBusy = true;
+                DI.Wrapper.SerialUsbConnect(
+                    this.selectedSerial,
+                    (err) => {
+                        this.ui.IsBusy = false;
+                        App.ShowMsg(err);
+                    });
+            }
+        }
+
+
+        private void OnUiDisconnect(object sender, EventArgs e) {
+            DI.Wrapper.SerialUsbDisconnect();
+        }
+
+
+        private void OnUiSend(object sender, string msg) {
+            DI.Wrapper.SerialUsbSend(msg);
+        }
+
+        #endregion
+
+    }
+}
---
+++ MultiCommTerminal/WindowObjs/SerialWins/SerialSelectUSB.xaml.cs
@@ -0,0 +1,100 @@
+﻿using LanguageFactory.Net.data;
+using MultiCommTerminal.NetCore.DependencyInjection;
+using MultiCommTerminal.NetCore.WPF_Helpers;
+using SerialCommon.Net.DataModels;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using WpfHelperClasses.Core;
+
+namespace MultiCommTerminal.NetCore.WindowObjs.SerialWins {
+
+    /// <summary>Interaction logic for SerialSelectUSB.xaml</summary>
+    public partial class SerialSelectUSB : Window {
+
+        private Window parent = null;
+
+        public SerialDeviceInfo SerialInfo { get; private set; } = null;
+
+
+        public static SerialDeviceInfo ShowBox(Window parent) {
+            SerialSelectUSB win = new SerialSelectUSB(parent);
+            win.ShowDialog();
+            return win.SerialInfo;
+        }
+
+
+        public SerialSelectUSB(Window parent) {
+            this.parent = parent;
+            InitializeComponent();
+            WPF_ControlHelpers.CenterChild(parent, this);
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+        }
+
+
+        /// <summary>Connect the style mouse grab on title bar</summary>
+        public override void OnApplyTemplate() {
+            this.BindMouseDownToCustomTitleBar();
+            base.OnApplyTemplate();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            WPF_ControlHelpers.CenterChild(parent, this);
+            DI.Wrapper.SerialDiscoveredDevices += this.discoveredDevices;
+            DI.Wrapper.SerialOnError += this.onSerialOnError;
+            this.gridWait.Show();
+            DI.Wrapper.SerialUsbDiscoverAsync();
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            DI.Wrapper.SerialDiscoveredDevices -= this.discoveredDevices;
+            DI.Wrapper.SerialOnError -= this.onSerialOnError;
+            this.lbUsb.SelectionChanged -= this.usbSelectionChanged;
+        }
+
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+
+        #region Event handlers
+
+        private void onSerialOnError(object sender, SerialUsbError e) {
+            this.Dispatcher.Invoke(() => {
+                this.gridWait.Collapse();
+                App.ShowMsgTitle(e.PortName, string.Format("{0} '{1}'", e.Code, e.Message));
+                this.Close();
+            });
+        }
+
+
+        private void discoveredDevices(object sender, List<SerialDeviceInfo> items) {
+            this.Dispatcher.Invoke(() => {
+                this.gridWait.Collapse();
+                if (items.Count == 0) {
+                    App.ShowMsgTitle("", DI.Wrapper.GetText(MsgCode.NotFound));
+                    this.Close();
+                }
+                else {
+                    this.lbUsb.ItemsSource = items;
+                    this.lbUsb.SelectionChanged += this.usbSelectionChanged;
+                }
+            });
+        }
+
+
+        private void usbSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            SerialDeviceInfo info = this.lbUsb.SelectedItem as SerialDeviceInfo;
+            if (info != null) {
+                this.SerialInfo = info;
+                this.Close();
+            }
+        }
+
+        #endregion
+
+    }
+}
