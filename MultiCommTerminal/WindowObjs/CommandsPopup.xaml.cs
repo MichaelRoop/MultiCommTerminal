7cf244ac27da78baa602511be3b947cd7b0eef65
michael@roop.ca <michael@roop.ca>
2020-12-30 15:47:54 -0400

    MUL-68 No Select on Terminator and Command popups

--- MultiCommTerminal/WindowObjs/CommandsPopup.xaml.cs
+++ MultiCommTerminal/WindowObjs/CommandsPopup.xaml.cs
@@ -1,6 +1,4 @@
-﻿using LanguageFactory.Net.data;
-using MultiCommData.Net.Enumerations;
-using MultiCommData.Net.StorageDataModels;
+﻿using MultiCommData.Net.Enumerations;
 using MultiCommTerminal.NetCore.DependencyInjection;
 using MultiCommTerminal.NetCore.WPF_Helpers;
 using StorageFactory.Net.interfaces;
@@ -16,8 +14,6 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
     public partial class CommandsPopup : Window {
 
         private Window parent = null;
-        private ButtonGroupSizeSyncManager widthManager = null;
-        private ScriptDataModel original = null;
         private CommMedium medium = CommMedium.None;
 
         public static void ShowBox(Window parent, CommMedium medium) {
@@ -31,10 +27,6 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
             this.medium = medium;
             InitializeComponent();
             this.SizeToContent = SizeToContent.WidthAndHeight;
-
-            // Call before rendering which will trigger initial resize events
-            this.widthManager = new ButtonGroupSizeSyncManager(this.btnCancel, this.btnSelect);
-            this.widthManager.PrepForChange();
         }
 
 
@@ -45,7 +37,6 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
 
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-            DI.Wrapper.GetSettings(this.OnGetOk, this.OnGetError);
             DI.Wrapper.GetScriptList(this.LoadList, this.OnGetError);
             this.lbxCmds.SelectionChanged += this.selectionChangedHandler;
             WPF_ControlHelpers.CenterChild(parent, this);
@@ -58,58 +49,13 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
 
 
         private void btnCancel_Click(object sender, RoutedEventArgs e) {
-            DI.Wrapper.SetCurrentScript(this.original, this.medium, this.OnGetError);
-            this.Close();
-        }
-
-
-        private void btnSelect_Click(object sender, RoutedEventArgs e) {
             this.Close();
         }
 
 
         private void selectionChangedHandler(object sender, SelectionChangedEventArgs e) {
             IIndexItem<DefaultFileExtraInfo> item = this.lbxCmds.SelectedItem as IIndexItem<DefaultFileExtraInfo>;
-            DI.Wrapper.SetCurrentScript(item, this.medium, () => { }, App.ShowMsg);
-        }
-
-
-        private void OnGetOk(SettingItems settings) {
-            if (settings == null) {
-                this.OnGetError("NULL COMMAND SET");
-                return;
-            }
-
-            this.original = settings.CurrentScript;
-            switch (this.medium) {
-                case CommMedium.Bluetooth:
-                    this.original = settings.CurrentScriptBT;
-                    break;
-                case CommMedium.BluetoothLE:
-                    this.original = settings.CurrentScriptBLE;
-                    break;
-                case CommMedium.Ethernet:
-                    this.original = settings.CurrentScriptEthernet;
-                    break;
-                case CommMedium.Usb:
-                    this.original = settings.CurrentScriptUSB;
-                    break;
-                case CommMedium.Wifi:
-                    this.original = settings.CurrentScriptWIFI;
-                    break;
-                default:
-                    this.original = settings.CurrentScript;
-                    break;
-            }
-
-            if (this.original == null) {
-                this.original = settings.CurrentScript;
-            }
-
-            // This would be an error where both the specific and default is null. So, a read failure
-            if (this.original == null) {
-                this.OnGetError(DI.Wrapper.GetText(MsgCode.ReadFailure));
-            }
+            DI.Wrapper.SetCurrentScript(item, this.medium, this.Close, App.ShowMsg);
         }
 
 
--- MultiCommTerminal/WindowObjs/TerminatorDataSelectorPopup.xaml.cs
+++ MultiCommTerminal/WindowObjs/TerminatorDataSelectorPopup.xaml.cs
@@ -1,6 +1,4 @@
-﻿using LanguageFactory.Net.data;
-using MultiCommData.Net.Enumerations;
-using MultiCommData.Net.StorageDataModels;
+﻿using MultiCommData.Net.Enumerations;
 using MultiCommTerminal.NetCore.DependencyInjection;
 using MultiCommTerminal.NetCore.WPF_Helpers;
 using StorageFactory.Net.interfaces;
@@ -15,8 +13,6 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
     public partial class TerminatorDataSelectorPopup : Window {
 
         private Window parent = null;
-        private ButtonGroupSizeSyncManager widthManager = null;
-        private TerminatorDataModel original = null;
         private CommMedium medium = CommMedium.None;
 
         #region Constructors and window events
@@ -32,10 +28,6 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
             this.medium = medium;
             InitializeComponent();
             this.SizeToContent = SizeToContent.WidthAndHeight;
-
-            // Call before rendering which will trigger initial resize events
-            this.widthManager = new ButtonGroupSizeSyncManager(this.btnCancel, this.btnSelect);
-            this.widthManager.PrepForChange();
         }
 
 
@@ -45,34 +37,26 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-            // Get the original in case of cancel
-            DI.Wrapper.GetSettings(this.GetOriginal, this.HandleError);
             this.LoadList();
             WPF_ControlHelpers.CenterChild(parent, this);
         }
 
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
-            this.widthManager.Teardown();
+            this.listBoxTerminators.SelectionChanged -= this.listBoxTerminators_SelectionChanged;
         }
 
         #endregion
 
         private void btnCancel_Click(object sender, RoutedEventArgs e) {
-            DI.Wrapper.SetCurrentTerminators(this.original, this.medium, this.HandleError);
-            this.Close();
-        }
-
-
-        private void btnSelect_Click(object sender, RoutedEventArgs e) {
             this.Close();
         }
 
 
         private void listBoxTerminators_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             var item = this.listBoxTerminators.SelectedItem as IIndexItem<DefaultFileExtraInfo>;
-            DI.Wrapper.SetCurrentTerminators(item, this.medium, () => { }, App.ShowMsg);
+            DI.Wrapper.SetCurrentTerminators(item, this.medium, this.Close, App.ShowMsg);
         }
-
 
         #region Private
 
@@ -86,47 +70,7 @@ namespace MultiCommTerminal.NetCore.WindowObjs {
             this.listBoxTerminators.SelectionChanged += this.listBoxTerminators_SelectionChanged;
         }
 
-
-        private void GetOriginal(SettingItems settings) {
-            switch (this.medium) {
-                case CommMedium.Bluetooth:
-                    this.original = settings.CurrentTerminatorBT;
-                    break;
-                case CommMedium.BluetoothLE:
-                    this.original = settings.CurrentTerminatorBLE;
-                    break;
-                case CommMedium.Ethernet:
-                    this.original = settings.CurrentTerminatorEthernet;
-                    break;
-                case CommMedium.Usb:
-                    this.original = settings.CurrentTerminatorUSB;
-                    break;
-                case CommMedium.Wifi:
-                    this.original = settings.CurrentTerminatorWIFI;
-                    break;
-                default:
-                    break;
-            }
-
-            // Default with a know terminator set
-            if (this.original == null) {
-                this.original = settings.CurrentTerminator;
-            }
-
-            // This would be an error where both the specific and default is null. So, a read failure
-            if (this.original == null) {
-                this.HandleError(DI.Wrapper.GetText(MsgCode.ReadFailure));
-            }
-        }
-
-        private void HandleError(string err) {
-            App.ShowMsg(err);
-            this.Close();
-        }
-
-
         #endregion
-
 
     }
 }
