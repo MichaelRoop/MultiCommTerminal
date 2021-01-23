362edcc99baf592bcc696ecf6c7e25155e341d6c
michael@roop.ca <michael@roop.ca>
2021-01-23 19:45:59 -0400

    MUL-83 fix crash opening email

--- MultiCommTerminal/WindowObjs/Utils/CrashReport.xaml.cs
+++ MultiCommTerminal/WindowObjs/Utils/CrashReport.xaml.cs
@@ -78,6 +78,7 @@ namespace MultiCommTerminal.NetCore.WindowObjs.Utils {
                     .Append(DateTime.Now.ToLongDateString()).Append("%0d%0A").Append("%0d%0A")
                     .Append(body).Append("%0d%0A");
                 Process.Start(new ProcessStartInfo(sb.ToString()) { UseShellExecute = true });
+                // this.DoEmail();
             }
             catch (Exception) {
             }
@@ -99,7 +100,7 @@ namespace MultiCommTerminal.NetCore.WindowObjs.Utils {
                     else {
                         report = WrapErr.GetErrReport(0, e.Message, e);
                     }
-                    this.errBox.Text = string.Format("{0}\r\n{1}", erex.Report.Msg,erex.Report.StackTrace);
+                    this.errBox.Text = string.Format("{0}\r\n{1}", report.Msg, report.StackTrace);
                 }
                 else {
                     this.errBox.Text = "Null exception. No info";
@@ -109,6 +110,49 @@ namespace MultiCommTerminal.NetCore.WindowObjs.Utils {
                 this.errBox.Text = "Failed to populate";
             }
         }
+
+        #region Experimental code
+#if THISDOESNOTWORK
+        private void DoEmail() {
+
+            StringBuilder sb = new StringBuilder();
+            sb
+                .Append("&body=App Build numberXXX:").Append(TxtBinder.BuildNumber).Append("%0d%0A").Append("%0d%0A")
+                .Append(DateTime.Now.ToLongDateString()).Append("%0d%0A").Append("%0d%0A")
+                .Append(this.errBox.Text.Replace("\r\n", "%0d%0A")).Append("%0d%0A");
+            string body = sb.ToString();
+
+            // This does not work. Suspend for now. App store would not approve either. More work to do
+            // https://social.msdn.microsoft.com/Forums/windowsapps/en-US/d4e48c03-33e8-4bd2-b0f1-fd68100a2c04/uwpsend-emails-through-outlook-from-uwp-out-of-process-background-task?forum=wpdevelop
+            EmailMessage emailMsg = new Windows.ApplicationModel.Email.EmailMessage();
+            emailMsg.Subject = "MultiCommTerminal CRASH REPORT";
+            emailMsg.Body = body;
+            EmailRecipient recipient = new EmailRecipient("MultiCommTerminal@gmail.com", "Support");
+            emailMsg.To.Add(recipient);
+            AutoResetEvent done = new AutoResetEvent(false);
+
+            Task.Run(async () => {
+                await Windows.ApplicationModel.Core.CoreApplication.MainView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    async () => {
+                        try {
+                            await Windows.ApplicationModel.Email.EmailManager.ShowComposeNewEmailAsync(emailMsg);
+                            done.Set();
+                            App.ShowMsg("Done opening email");
+                        }
+                        catch (Exception e) {
+                            App.ShowMsg("Crash opening email\r\n\r\n" + e.Message);
+                            done.Set();
+                        }
+                    });
+            });
+
+            App.ShowMsg("Waiting");
+            done.WaitOne();
+            App.ShowMsg("Done Waiting");
+
+        }
+#endif
+        #endregion
 
         #endregion
 
