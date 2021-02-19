f87f12d2da1d99ad66f375b97a85178546426f2d
michael@roop.ca <michael@roop.ca>
2021-02-18 22:21:06 -0400

    MUL-78 Appearance Characteristic translate structure

--- MultiCommWrapper/WrapCode/CommWrapper_BluetoothLE_Translate.cs
+++ MultiCommWrapper/WrapCode/CommWrapper_BluetoothLE_Translate.cs
@@ -1,5 +1,6 @@
 ﻿using BluetoothLE.Net.DataModels;
 using BluetoothLE.Net.Enumerations;
+using BluetoothLE.Net.Parsers.Characteristics;
 using BluetoothLE.Net.Parsers.Descriptor;
 using BluetoothLE.Net.Tools;
 using LanguageFactory.Net.data;
@@ -77,6 +78,12 @@ namespace MultiCommWrapper.Net.WrapCode {
                 }
             }
 
+            if (dataModel.Parser is CharParser_Appearance) {
+
+                //dataModel.DisplayHeader = 
+                dataModel.CharValue = this.Translate(dataModel.Parser as CharParser_Appearance);
+            }
+
             foreach (BLE_DescriptorDataModel d in dataModel.Descriptors) {
                 this.Translate(d);
             }
@@ -144,6 +151,15 @@ namespace MultiCommWrapper.Net.WrapCode {
         }
 
 
+        private string Translate(CharParser_Appearance p) {
+            p.Translate(() => {
+                return this.TranslateAppearance(
+                    p.Category, p.SubCategory, p.DisplayString);
+            });
+            return p.DisplayString;
+        }
+
+
         private string Translate(UnitsOfMeasurement unit) {
             // First check for string returned if they are handled lower
             string value = unit.ToStr();
@@ -178,6 +194,132 @@ namespace MultiCommWrapper.Net.WrapCode {
             }
         }
 
+
+        private string TranslateAppearance(uint category, uint sub, string existing) {
+            BLE_AppearanceCategory cat = category.FirstOrDefault(BLE_AppearanceCategory.Unknown);
+            if (cat == BLE_AppearanceCategory.Unknown) {
+                return existing;
+            }
+
+            // Some categories only have one entry
+            // when we find it we can process and return
+            switch (cat) {
+                case BLE_AppearanceCategory.Unknown:
+                    return existing;
+
+                // Entries with no sub categories
+                case BLE_AppearanceCategory.Phone:
+                case BLE_AppearanceCategory.Computer:
+                case BLE_AppearanceCategory.Clock:
+                case BLE_AppearanceCategory.Display:
+                case BLE_AppearanceCategory.Remote_Control:
+                case BLE_AppearanceCategory.Eye_Glasses:
+                case BLE_AppearanceCategory.Tag:
+                case BLE_AppearanceCategory.Key_Ring:
+                case BLE_AppearanceCategory.Media_Player:
+                case BLE_AppearanceCategory.Barcode_Scanner:
+                case BLE_AppearanceCategory.Thermometer:
+                case BLE_AppearanceCategory.Glucose_Meter:
+                case BLE_AppearanceCategory.Network_Device:
+                case BLE_AppearanceCategory.Air_Conditioning:
+                case BLE_AppearanceCategory.Humidifier:
+                case BLE_AppearanceCategory.Weight_Scale:
+                    return string.Format("{0} {1}", existing, cat.ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Watch:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceHeartRate.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Heart_Rate:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceWatch.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Blood_Pressure:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceBloodPressure.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Human_Interface_Device:
+                    return string.Format("{0} {1}",
+                        existing,
+                        //cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceHID.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Run_Walk_Sensor:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceRunWalkSensor.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Cycling:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceCycling.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Control_Device:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceControlDevice.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Sensor:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceSensor.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Light_Fixture:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceLightFixture.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Fan:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceFan.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.HVAC:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceHVAC.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Heating:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceHeating.Generic).ToString().UnderlineToSpaces());
+                
+                case BLE_AppearanceCategory.Access_Control:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceAccessControl.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Motorized_Device:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceMotorizedDevice.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Power_Device:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearancePowerDevice.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Light_Source:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceLightSource.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Oximeter:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceOximeter.Generic).ToString().UnderlineToSpaces());
+                case BLE_AppearanceCategory.Outdoor_Sports_Activity:
+                    return string.Format("{0} {1} : {2}",
+                        existing,
+                        cat.ToString().UnderlineToSpaces(),
+                        sub.FirstOrDefault(BLE_AppearanceOutdoorSportActivity.Generic).ToString().UnderlineToSpaces());
+                default:
+                    return existing;
+            }
+        }
 
     }
 
