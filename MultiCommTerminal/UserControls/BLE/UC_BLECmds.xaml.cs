cc053de49c02ef904799d5d18e24c7c38141686e
michael@roop.ca <michael@roop.ca>
2021-03-11 10:15:57 -0400

    BLE Cmds User Control

---
+++ MultiCommTerminal/UserControls/BLE/UC_BLECmds.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiCommTerminal.NetCore.UserControls.BLE {
+    /// <summary>
+    /// Interaction logic for UC_BLECmds.xaml
+    /// </summary>
+    public partial class UC_BLECmds : UserControl {
+        public UC_BLECmds() {
+            InitializeComponent();
+        }
+    }
+}
