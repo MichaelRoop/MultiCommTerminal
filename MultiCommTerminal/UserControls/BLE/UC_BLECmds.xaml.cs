2a0e3f705a1c01555c2aae556bba27ddb5a4d661
michael@roop.ca <michael@roop.ca>
2021-03-18 17:53:20 -0300

    MUL-113 command list simple list

--- MultiCommTerminal/UserControls/BLE/UC_BLECmds.xaml.cs
+++ MultiCommTerminal/UserControls/BLE/UC_BLECmds.xaml.cs
@@ -1,20 +1,13 @@
 ﻿using BluetoothLE.Net.DataModels;
+using LogUtils.Net;
 using MultiCommData.Net.StorageDataModels;
 using MultiCommData.Net.StorageIndexInfoModels;
 using MultiCommTerminal.NetCore.DependencyInjection;
 using StorageFactory.Net.interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfHelperClasses.Core;
 
 namespace MultiCommTerminal.NetCore.UserControls.BLE {
@@ -22,16 +15,18 @@ namespace MultiCommTerminal.NetCore.UserControls.BLE {
     /// <summary>Interaction logic for UC_BLECmds.xaml</summary>
     public partial class UC_BLECmds : UserControl {
 
-
-        //private BLECommandSetDataModel general = null;
-        //private BLECommandSetDataModel specific = null;
+        #region Data
 
         private List<IIndexItem<BLECmdIndexExtraInfo>> general = new List<IIndexItem<BLECmdIndexExtraInfo>>();
-        private List<IIndexItem<BLECmdIndexExtraInfo>> specific = new List<IIndexItem<BLECmdIndexExtraInfo>>();
         private BLE_CharacteristicDataModel characteristic = null;
+        private string currentCmd = string.Empty;
+        private ClassLog log = new ClassLog("UC_BLECmds");
+
+        #endregion
+
+        #region Public
 
         public event EventHandler<string> OnCmdSelected;
-
 
         public UC_BLECmds() {
             InitializeComponent();
@@ -40,80 +35,105 @@ namespace MultiCommTerminal.NetCore.UserControls.BLE {
 
 
         public void Init(BLE_CharacteristicDataModel dm) {
-            // The BLE tree updates continuously so need to check if same
-            if (this.characteristic != null && this.characteristic.Uuid == dm.Uuid) {
-                return;
-            }
-
-            this.Unsubscribe();
-            this.lvBLECmdList.ItemsSource = null;
-            this.lb_BLECmds.ItemsSource = null;
-            this.lblCmdDataTypeContent.Content = "";
-            this.characteristic = dm;
-            if (this.characteristic.IsWritable) {
-                this.lblCmdDataTypeContent.Content = dm.DataTypeDisplay;
-                DI.Wrapper.GetFilteredBLECmdList(
-                    this.characteristic.Parser.DataType,
-                    this.characteristic.CharName,
-                    (general, specific) => {
-                        this.general = general;
-                        this.specific = specific;
-                        // We would do a check if specific exists then it becomes list and enable filter icon if general also has number > 1
-
-                        // TODO - for now just show general. Need filter swap in UI if specific exists
-                        this.lvBLECmdList.ItemsSource = this.general;
-                        //this.ForceCmdListResize();
-                        this.lvBLECmdList.ForceColumnResize();
-
-                    }, App.ShowMsg);
-            }
-            this.Subscribe();
-            if (this.lvBLECmdList.Items != null && this.lvBLECmdList.Count() > 0) {
-                this.lvBLECmdList.SelectedIndex = 0;
-                // Then select first of commands
-                if (this.lb_BLECmds.Items != null && this.lb_BLECmds.Count() > 0) {
-                    this.lb_BLECmds.SelectedIndex = 0;
+            try {
+                // The BLE tree updates continuously so need to check if same
+                if (this.characteristic != null && this.characteristic.Uuid == dm.Uuid) {
+                    return;
                 }
 
+                this.Unsubscribe();
+                this.lbCmdList.ItemsSource = null;
+                this.lb_BLECmds.ItemsSource = null;
+                this.lblCmdDataTypeContent.Content = "";
+                this.characteristic = dm;
+                if (this.characteristic.IsWritable) {
+                    this.lblCmdDataTypeContent.Content = dm.DataTypeDisplay;
+                    DI.Wrapper.GetFilteredBLECmdList(
+                        this.characteristic.Parser.DataType,
+                        this.characteristic.CharName,
+                        (general, specific) => {
+                        // Only using general for now. Not filtering on specific characteristic name
+                        this.general = general;
+                            this.lbCmdList.ItemsSource = this.general;
+                        }, App.ShowMsg);
+                }
+                this.Subscribe();
+                if (this.lbCmdList.Items != null && this.lbCmdList.Count() > 0) {
+                    this.lbCmdList.SelectedIndex = 0;
+                    // Then select first of commands
+                    if (this.lb_BLECmds.Items != null && this.lb_BLECmds.Count() > 0) {
+                        this.lb_BLECmds.SelectedIndex = 0;
+                    }
+                }
+            }
+            catch (Exception ex) {
+                this.log.Exception(9999, "Init", "", ex);
             }
         }
 
+        #endregion
 
         #region Event handling
 
         private void Subscribe() {
-            this.lvBLECmdList.SelectionChanged += this.cmdListSelectionChanged;
+            this.lbCmdList.SelectionChanged += this.lbCmdList_SelectionChanged;
             this.lb_BLECmds.SelectionChanged += this.cmdsSelectionChanged;
         }
 
 
         private void Unsubscribe() {
-            this.lvBLECmdList.SelectionChanged -= this.cmdListSelectionChanged;
+            this.lbCmdList.SelectionChanged -= this.lbCmdList_SelectionChanged;
             this.lb_BLECmds.SelectionChanged -= this.cmdsSelectionChanged;
         }
 
 
-        private void cmdListSelectionChanged(object sender, SelectionChangedEventArgs e) {
-            this.lb_BLECmds.SelectionChanged -= this.cmdsSelectionChanged;
-            this.lb_BLECmds.ItemsSource = null;
-            //this.current = null;
-
-
-            IIndexItem<BLECmdIndexExtraInfo> item = this.lvBLECmdList.SelectedItem as IIndexItem<BLECmdIndexExtraInfo>;
-            if (item != null) {
-                DI.Wrapper.RetrieveBLECmdSet(item, dm => {
-                    //this.current = dm;
-                    this.lb_BLECmds.ItemsSource = dm.Items;
-                }, App.ShowMsg);
+        private void lbCmdList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            try {
+                this.lb_BLECmds.SelectionChanged -= this.cmdsSelectionChanged;
+                this.lb_BLECmds.ItemsSource = null;
+                DI.Wrapper.RetrieveBLECmdSet(
+                    this.lbCmdList.SelectedItem as IIndexItem<BLECmdIndexExtraInfo>,
+                    this.OnCmdListChanged,
+                    App.ShowMsg);
             }
-            this.lb_BLECmds.SelectionChanged += this.cmdsSelectionChanged;
+            catch (Exception ex) {
+                this.log.Exception(9999, "lbCmdList_SelectionChanged", "", ex);
+            }
+            finally {
+                this.lb_BLECmds.SelectionChanged += this.cmdsSelectionChanged;
+            }
         }
 
 
         private void cmdsSelectionChanged(object sender, SelectionChangedEventArgs e) {
-            ScriptItem item = this.lb_BLECmds.SelectedItem as ScriptItem;
-            if (item != null) {
-                this.OnCmdSelected?.Invoke(this, item.Command);
+            try {
+                ScriptItem item = this.lb_BLECmds.SelectedItem as ScriptItem;
+                if (item != null) {
+                    this.currentCmd = item.Command;
+                    if (this.IsVisible) {
+                        this.OnCmdSelected?.Invoke(this, this.currentCmd);
+                    }
+                }
+            }
+            catch(Exception ex) {
+                this.log.Exception(9999, "cmdsSelectionChanged", "", ex);
+            }
+        }
+
+
+        private void UC_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args) {
+            if (this.IsVisible && this.currentCmd != string.Empty) {
+                this.OnCmdSelected?.Invoke(this, this.currentCmd);
+            }
+            else {
+                this.OnCmdSelected?.Invoke(this, string.Empty);
+            }
+        }
+
+
+        private void OnCmdListChanged(BLECommandSetDataModel dm) {
+            if (dm != null) {
+                this.lb_BLECmds.ItemsSource = dm.Items;
             }
         }
 
